e4ff9307984c388e9c19ccea4311e75c52648b1e
Federica Cestari <federica.cestari@gmail.com>
2021-02-12 17:36:03 +0100

    Update uninstall-ivar.sh

--- scripts/uninstall-ivar.sh
+++ scripts/uninstall-ivar.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-PASS="pippo";
-
+echo "Run as root. *MAY* damage your server.";
+echo -n "Do you want to proceed ? crl-C to abort, Enter to continue";
+read ANSWER;
 deluser web2py; 
 rm -rf /home/web2py 
 apt remove --purge -y python-minimal
 apt remove --purge -y python-pip
 apt remove --purge -y unzip
-# echo "revoke all privileges, grant option from 'ivar'@'localhost'; flush privileges;" | mysql -u root -pR$PASS
-# echo "drop database ivar;" | mysql -u root -pR$PASS
 dpkg --purge mariadb-client mariadb-common mariadb-client-core-10.1 mariadb-client-10.1 mariadb-server-core-10.1 mariadb-server-10.1 mariadb-server
-# apt remove --purge -y mariadb-server mariadb-client
 apt autoremove -y
-
