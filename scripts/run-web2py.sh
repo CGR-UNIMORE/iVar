fe87b3697fa73dc8673737c0f40f7c212217d830
Giovanni Faglioni <giova@nabla2.it>
2021-02-12 15:36:10 +0000

    x bit added to script

--- scripts/run-web2py.sh
+++ scripts/run-web2py.sh
@@ -3,7 +3,7 @@
 /bin/su - web2py -c "/usr/bin/python \
         ./web2py/web2py.py \
         -i 0.0.0.0 -p 9000 \
-        -a PASSWORD \
+        -a pippo \
         -c ./iVar/private/ivar-cert.pem \
         -k ./iVar/private/ivar-privkey.pem \
         2>&1 >> log/web2py.log-$(/bin/date +%Y%m%d-%H%M%S) 2>&1 &"
