e6300a60badb3e59c7b63293fe3264fd5299945c
Giovanni Faglioni <giova@nabla2.it>
2021-02-12 15:33:12 +0000

    Run script added.

---
+++ scripts/run-web2py.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+  
+/bin/su - web2py -c "/usr/bin/python \
+        ./web2py/web2py.py \
+        -i 0.0.0.0 -p 9000 \
+        -a PASSWORD \
+        -c ./iVar/private/ivar-cert.pem \
+        -k ./iVar/private/ivar-privkey.pem \
+        2>&1 >> log/web2py.log-$(/bin/date +%Y%m%d-%H%M%S) 2>&1 &"
